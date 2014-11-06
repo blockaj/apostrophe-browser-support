79f6fc322fca0e6364bbc2ea019d598b739bbe59
Aaron <ajblock@gmail.com>
2014-11-06 14:37:03 -0500

    Detect name and version

--- public/js/browserdetect.js
+++ public/js/browserdetect.js
@@ -1,6 +1,12 @@
 // var req = jQuery.get("/apos/browser-support?browser=" + platform.name + "&version=" + platform.version);
-console.log(apos.Data);
-var blacklist = apos.data.blacklist;
-if (platform.name == blacklist) {
-  console.log('You are using a terrible browser!');
-}
+apos.on('ready', function() {
+	console.log(apos.data.blacklist);
+	var blacklist = apos.data.blacklist;
+	var browserVersion = parseFloat(platform.version);
+	for (var i = 0; i < blacklist.length; i++) {
+		if (platform.name == blacklist[i].name && browserVersion == blacklist[i].version) {
+			console.log('Stop you old fuck!');
+		}
+	}
+});
+
