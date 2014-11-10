a3557aea7da621e0002f5651925790cedab80805
Aaron <ajblock@gmail.com>
2014-11-10 09:37:40 -0500

    Attempting to identify templates and hide the right ones

--- public/js/browserdetect.js
+++ public/js/browserdetect.js
@@ -1,12 +1,19 @@
-// var req = jQuery.get("/apos/browser-support?browser=" + platform.name + "&version=" + platform.version);
 apos.on('ready', function() {
+	$('.apos-refreshable').css('display', 'none');
+	$('.oldbrowser').css('display', 'none');
 	console.log(apos.data.blacklist);
 	var blacklist = apos.data.blacklist;
 	var browserVersion = parseFloat(platform.version);
+
+	//Is there a better way to do this? I don't know one. 
 	for (var i = 0; i < blacklist.length; i++) {
 		if (platform.name == blacklist[i].name && browserVersion == blacklist[i].version) {
-			console.log('Stop you old fuck!');
+			console.log('Stop');
+			//$('.apos-refreshable').css('display', 'none');
+			$('oldbrowser').css('display', 'block');
+		} else {
+			$('body').css('display', 'block');
+			//$('.oldbrowser').css('display', 'none');
 		}
 	}
 });
-
