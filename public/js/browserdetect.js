0876a8b65f4b6a32f1e31fef3328237998ce3cb1
Aaron <ajblock@gmail.com>
2014-11-20 15:01:24 -0500

    Looks for includeOlder option for graylist

--- public/js/browserdetect.js
+++ public/js/browserdetect.js
@@ -3,9 +3,11 @@ var graylist = window.graylist;
 console.log(blacklist);
 console.log(graylist);
 for (var i = 0; i < blacklist.length; i++) {
-	var browser = blacklist[i].name,
+	var browser = blacklist[i].name.toLowerCase(),
 	    version = blacklist[i].version,
+	    platformName = platform.name.toLowerCase(),
 	    ignoreOlder = blacklist[i].ignoreOlder;
+
 	if (ignoreOlder) {
 		if (browser == platform.name && parseFloat(platform.version) == version) {
 			if (window.location.href != '/apos-browser-support/old-browser') {
@@ -24,13 +26,26 @@ for (var i = 0; i < blacklist.length; i++) {
 for (var i = 0; i < graylist.length; i++) {
 	var browser = graylist[i].name.toLowerCase(),
 		version = graylist[i].version,
-		platformName = platform.name.toLowerCase();
-	if (browser == platformName && version == parseFloat(platform.version)) {
-		$(document).ready(function(){
-			$('.graylist-modal-outer').show();
-			$('#x-button').click(function(){
-				$('.graylist-modal-outer').hide();
-			});
-		});
+		platformName = platform.name.toLowerCase(),
+		includeOlder = graylist[i].includeOlder;
+
+	if (includeOlder) {
+		if (browser == platformName && version <= parseFloat(platform.version)) {
+			hideModal();
+		}
+	} else {
+		if (browser == platformName && version == parseFloat(platform.version)) {
+			hideModal();
+		}	
 	}
+	
+}
+
+function hideModal() {
+	$(document).ready(function(){
+		$('.graylist-modal-outer').show();
+		$('#x-button').click(function(){
+			$('.graylist-modal-outer').hide();
+		});
+	});
 }
