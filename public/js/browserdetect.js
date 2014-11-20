6fae2fdb6d68e2d86f6df94d6df0651b9fbf6d3c
Aaron <ajblock@gmail.com>
2014-11-20 14:53:56 -0500

    Fixed some case-sensitive issues

--- public/js/browserdetect.js
+++ public/js/browserdetect.js
@@ -1,6 +1,7 @@
 var blacklist = window.blacklist;
 var graylist = window.graylist;
 console.log(blacklist);
+console.log(graylist);
 for (var i = 0; i < blacklist.length; i++) {
 	var browser = blacklist[i].name,
 	    version = blacklist[i].version;
@@ -14,8 +15,7 @@ for (var i = 0; i < graylist.length; i++) {
 	var browser = graylist[i].name.toLowerCase(),
 		version = graylist[i].version,
 		platformName = platform.name.toLowerCase();
-		
-	if (browser == platform.name && version == parseFloat(platform.version)) {
+	if (browser == platformName && version == parseFloat(platform.version)) {
 		$(document).ready(function(){
 			$('.graylist-modal-outer').show();
 			$('#x-button').click(function(){
